4c3df10e6bb81645593d5545efce5544d31ea18b
Yosuke Tomita <yosuke.tomita@gengo.com>
2016-02-09 11:01:05 +0900

    wrapper for alert conditions update end-point

--- newrelic_api/alert_conditions.py
+++ newrelic_api/alert_conditions.py
@@ -1,4 +1,5 @@
 from .base import Resource
+from newrelic_api.exceptions import NoEntityException
 
 
 class AlertConditions(Resource):
@@ -68,3 +69,98 @@ class AlertConditions(Resource):
             headers=self.headers,
             params=self.build_param_string(filters)
         )
+
+    def update(
+            self, policy_id, alert_condition_id,
+            condition_type=None,
+            name=None,
+            enabled=None,
+            entities=None,
+            metric=None,
+            terms=None):
+        """
+        Updates any of the optional parameters of the alert condition
+
+        :type policy_id: int
+        :param policy_id: Alert policy id where target alert condition belongs to
+
+        :type alert_condition_id: int
+        :param alert_condition_id: Alerts condition id to update
+
+        :type name: str
+        :param name: The name of the server
+
+        :type enabled: bool
+        :param enabled: Whether to enable that alert condition
+
+        :type entities: list[str]
+        :param name: entity ids to which the alert condition is applied
+
+        :rtype: dict
+        :return: The JSON response of the API
+
+        :raises: This will raise a
+            :class:`NewRelicAPIServerException<newrelic_api.exceptions.NoEntityException>`
+            if target alert condition is not included in target policy
+
+        ::
+
+            {
+                "condition": {
+                    "id": "integer",
+                    "condition_type": "string",
+                    "name": "string",
+                    "enabled": "boolean",
+                    "entities": [
+                        "integer"
+                    ],
+                    "metric": "string",
+                    "runbook_url": "string",
+                    "terms": [
+                        {
+                            "duration": "string",
+                            "operator": "string",
+                            "priority": "string",
+                            "threshold": "string",
+                            "time_function": "string"
+                        }
+                    ],
+                    "user_defined": {
+                        "metric": "string",
+                        "value_function": "string"
+                    }
+                }
+            }
+
+        """
+        conditions_dict = self.list(policy_id)
+        target_condition = None
+        for condition in conditions_dict['conditions']:
+            if int(condition['id']) == alert_condition_id:
+                target_condition = condition
+                break
+
+        if target_condition is None:
+            raise NoEntityException(
+                'Target alert condition is not included in that policy.'
+                'policy_id: {}, alert_condition_id {}'.format(policy_id, alert_condition_id))
+
+        data = {
+            'condition': {
+                'condition_type': condition_type or target_condition['condition_type'],
+                'name': name or target_condition['name'],
+                'enabled': enabled or target_condition['enabled'],
+                'entities': entities or target_condition['entities'],
+                'metric': metric or target_condition['metric'],
+                'terms': terms or target_condition['terms'],
+            }
+        }
+
+        return self._put(
+            url='{0}alerts_conditions/{1}.json'.format(self.URL, alert_condition_id),
+            headers=self.headers,
+            data=data
+        )
+
+    # TODO: implement create and delete
+    # See https://docs.newrelic.com/docs/alerts/new-relic-alerts-beta/getting-started/rest-api-calls-new-relic-alerts
--- newrelic_api/tests/alert_conditions_tests.py
+++ newrelic_api/tests/alert_conditions_tests.py
@@ -4,17 +4,18 @@ from mock import patch, Mock
 import requests
 
 from newrelic_api.alert_conditions import AlertConditions
+from newrelic_api.exceptions import NoEntityException
 
 
 class NRAlertConditionsTests(TestCase):
     def setUp(self):
         super(NRAlertConditionsTests, self).setUp()
-        self.conditions = AlertConditions(api_key='dummy_key')
+        self.alert_conditions = AlertConditions(api_key='dummy_key')
 
-        self.conditions_list_response = {
+        self.list_success_response = {
             "conditions": [
                 {
-                    "id": "1",
+                    "id": "100",
                     "condition_type": "servers_metric",
                     "name": "CPU usage alert",
                     "enabled": True,
@@ -35,17 +36,39 @@ class NRAlertConditionsTests(TestCase):
             ]
         }
 
+        self.update_success_response = {
+            "condition": {
+                "id": "100",
+                "condition_type": "servers_metric",
+                "name": "CPU usage alert",
+                "enabled": True,
+                "entities": [
+                    "1234567"
+                ],
+                "metric": "cpu_percentage",
+                "terms": [
+                    {
+                        "duration": "5",
+                        "operator": "above",
+                        "priority": "above",
+                        "threshold": "90",
+                        "time_function": "all"
+                    }
+                ]
+            }
+        }
+
     @patch.object(requests, 'get')
     def test_list_success(self, mock_get):
         """
         Test alert conditions .list()
         """
         mock_response = Mock(name='response')
-        mock_response.json.return_value = self.conditions_list_response
+        mock_response.json.return_value = self.list_success_response
         mock_get.return_value = mock_response
 
         # Call the method
-        response = self.conditions.list(policy_id=1)
+        response = self.alert_conditions.list(policy_id=1)
 
         self.assertIsInstance(response, dict)
 
@@ -60,4 +83,56 @@ class NRAlertConditionsTests(TestCase):
 
         with self.assertRaises(ValueError):
             # Call the method
-            self.conditions.list(policy_id=1)
+            self.alert_conditions.list(policy_id=1)
+
+    @patch.object(requests, 'get')
+    @patch.object(requests, 'put')
+    def test_update_success(self, mock_put, mock_get):
+        """
+        Test alerts_conditions .update() success
+        """
+        mock_list_response = Mock(name='response')
+        mock_list_response.json.return_value = self.list_success_response
+        mock_update_response = Mock(name='response')
+        mock_update_response.json.return_value = self.update_success_response
+        mock_get.return_value = mock_list_response
+        mock_put.return_value = mock_update_response
+
+        # Call the method
+        response = self.alert_conditions.update(policy_id=1, alert_condition_id=100, name='New Name')
+
+        self.assertIsInstance(response, dict)
+
+    @patch.object(requests, 'get')
+    @patch.object(requests, 'put')
+    def test_update_failure(self, mock_put, mock_get):
+        """
+        Test alerts_conditions .update() failure
+        """
+        mock_list_response = Mock(name='response')
+        mock_list_response.json.return_value = self.list_success_response
+        mock_update_response = Mock(name='response')
+        mock_update_response.json.side_effect = ValueError('No JSON object could be decoded')
+        mock_get.return_value = mock_list_response
+        mock_put.return_value = mock_update_response
+
+        # Call the method
+        with self.assertRaises(ValueError):
+            self.alert_conditions.update(policy_id=1, alert_condition_id=100, name='New Name')
+
+    @patch.object(requests, 'get')
+    @patch.object(requests, 'put')
+    def test_update_no_alert_condition(self, mock_put, mock_get):
+        """
+        Test alerts_conditions .update() success
+        """
+        mock_list_response = Mock(name='response')
+        mock_list_response.json.return_value = self.list_success_response
+        mock_update_response = Mock(name='response')
+        mock_update_response.json.return_value = self.update_success_response
+        mock_get.return_value = mock_list_response
+        mock_put.return_value = mock_update_response
+
+        with self.assertRaises(NoEntityException):
+            # Call the method with non existing alert_condition_id
+            self.alert_conditions.update(policy_id=1, alert_condition_id=9999, name='New Name')
