6c7db15877c53344917a4f972b54911d31f04b3d
David Reed <david.reedfrancis@sainsburys.co.uk>
2018-07-30 15:22:47 +0100

    [GITHUB-1] Removes badges and renames to Sansible

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ def get_version():
 
 
 setup(
-    name='newrelic-api-gengo',
+    name='newrelic-api-sansible',
     version=get_version(),
     description='A python interface to the New Relic API v2',
     long_description=open('README.rst').read(),
-    url='https://github.com/gengo/newrelic-api',
+    url='https://github.com/sansible/newrelic-api',
     author='Micah Hausler',
     author_email='opensource@ambition.com',
     packages=find_packages(),
